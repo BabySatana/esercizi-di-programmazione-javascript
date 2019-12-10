88524bf784dcece158a8f710f78fcbccf03fa936
BabySatana <58731653+BabySatana@users.noreply.github.com>
2019-12-10 12:41:21 +0100

    Update conta-il-tempo.js

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -12,3 +12,29 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+var sec = 12560;
+
+var min = sec/60;
+var totM = Math.round(min);
+
+var hour = totM/60;
+var totH = Math.round(hour);
+
+var HinMinutes = totH*60; // 180m
+var HinSeconds = HinMinutes*60;
+
+var RemainingMin = totM-HinMinutes;
+console.log(RemainingMin); // 29 minuti rimanenti
+var Minseconds = RemainingMin*60;
+
+var SumSeconds = HinSeconds+Minseconds;
+
+var RemainingSeconds = sec-SumSeconds;
+
+
+
+
+
+console.log(`le ore sono ${totH}, i minuti sono ${RemainingMin} e i secondi rimasti sono ${RemainingSeconds}`);
